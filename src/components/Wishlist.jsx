ec3684bcb650709820a68f5310cf9b5974ca4c32
Hrishi <hrishikeshmane87933@gmail.com>
2022-03-31 11:46:54 +0530

    ref: colors changed to variables

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useProducts } from "../context/product-context";
 import Header from "./Header";
 import Rating from "./Rating";
